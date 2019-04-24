2568be078c03a547d191315e7bbf19a6547e55e2
Smith <to@ethicnolo.gy>
2019-04-24 12:41:18 +0200

    /register hashing password with bcrypt /login compare hashedPassword with bcrypt

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModels');
 const config = require('../../config/secrets');
+const bcrypt = require('bcrypt');
 
 exports.user_register = function(req, res) {
 	let new_user = new User(req.body);
 
 	new_user.save(function(err, user) {
 		if (err) res.send(err);
+		console.log("user = ", user);
 		res.json(user);
 	});
 };
@@ -14,7 +16,7 @@ exports.user_register = function(req, res) {
 exports.user_login = function(req, res) {
 	User.findOne({ email: req.body.email }, function(err, user) {
 		if (err) res.send(err);
-		if (user.email === req.body.email && user.password === req.body.password) {
+		if (user.email === req.body.email && bcrypt.compareSync(req.body.password, user.password) == true) {
 			jwt.sign(
 				{ user: user },
 				config.secrets.jwt_key,
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 var Schema = mongoose.Schema;
 var userschema = new Schema({
 	email: {
@@ -10,6 +13,13 @@ var userschema = new Schema({
 		type: String,
 		require: true
 	}
+});
+
+userschema.pre('save', function(next) {
+	console.log(this)
+  hashedPassword = bcrypt.hashSync(this.password, saltRounds);
+  this.password = hashedPassword;
+  next();
 });
 
 module.exports = mongoose.model('User', userschema);
